59cede1c83667be883718dc7392c9ab5c217dc7f
sitrucp <pokrant@yahoo.com>
2014-06-21 12:22:22 -0700

    Update DomaincontentController.php

--- DomaincontentController.php
+++ DomaincontentController.php
@@ -9,6 +9,8 @@ class DomaincontentsController extends AppController {
 		//retrieve data from model for current domain only
 		$this->set('domaincontent', $this->paginate(array('domain_id' => Configure::read('domain_id'))));
 		
+		//this filter can be used in any controller function that requirees domain specific data
+		//note that you must add/maintain domain_id foreign key to your tables where required
 		//this controller can be used as normal by View
 	}
 
